356e26f19c33908bec64cceff0042aff6932b9c9
bennygit9959 <bennycy93@gmail.com>
2023-01-17 14:03:31 -0500

    added a new line

--- main.js
+++ main.js
@@ -3,4 +3,6 @@ import { square, diag } from './lib.js';
 (function(){
   console.log(square(11)); // 121
   console.log(diag(4, 3)); // 5
+  console.log(diag(5, 2)); // 6
+  console.log(diag(4, 3)); // 5
 })()

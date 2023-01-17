e8198b4a573c783f6b83f5709381d010f55cd812
bennygit9959 <bennycy93@gmail.com>
2023-01-17 15:01:28 -0500

    Removed those lines

--- main.js
+++ main.js
@@ -3,7 +3,4 @@ import { square, diag } from './lib.js';
 (function(){
   console.log(square(11)); // 121
   console.log(diag(4, 3)); // 5
-  console.log(diag(5, 2)); // 6
-  console.log(diag(4, 3)); // 5
-console.log(diag(4, 3)); // 5 
 })()
